018326958dd4a03a7a455b6e2b129074a23521a7
Logan Hosoda <sulsalogan@gmail.com>
2024-04-05 16:19:43 -0700

    Update roman-numerals-encoder.js
    Completed Code Wars 7kyu Rank Up challenge - Roman Numerals Encoder

--- 6kyu/roman-numerals-encoder.js
+++ 6kyu/roman-numerals-encoder.js
@@ -21,3 +21,114 @@
 
 // SOLUTION
 
+const solution = number => {
+  let romanNum = "";
+  let arr = number.toString().split('').reverse().map(Number);
+  let mult = 1;
+  for (i = 0; i < arr.length; i++) {
+    arr[i] *= mult;
+    mult *= 10;
+  }
+  arr.reverse().forEach((element) => {
+    switch (true) {
+      case element.toString().length == 4:
+        romanNum += handleM(element);
+        break;
+      case element.toString().length == 3:
+        romanNum += handleC(element);
+        break;
+      case element.toString().length == 2:
+        romanNum += handleX(element);
+        break;
+      case element.toString().length == 1:
+        romanNum += handleI(element);
+        break;
+    }
+  })
+  
+  return romanNum;
+}
+
+const handleM = num => {
+  num /= 1000;
+  let romanNum = "";
+  while (num > 0) {
+    romanNum += "M";
+    num--;
+  }
+  return romanNum;
+}
+
+const handleC = num => {
+  if (num === 0) {return ""}
+  num /= 100;
+  let romanNum = "";
+  switch (true) {
+    case (num < 4):
+      romanNum += "C".repeat(num);
+      break;
+    case (num == 4):
+      romanNum += "CD";
+      break;
+    case (num == 5):
+      romanNum += "D";
+      break;
+    case (num > 5 && num < 9):
+      romanNum = "D";
+      romanNum += "C".repeat((num - 5));
+      break;
+    case (num == 9):
+      romanNum = "CM";
+      break;
+  }
+  return romanNum;
+}
+
+const handleX = num => {
+  if (num === 0) {return ""}
+  num /= 10;
+  let romanNum = "";
+  switch (true) {
+    case (num < 4):
+      romanNum += "X".repeat(num);
+      break;
+    case (num == 4):
+      romanNum += "XL";
+      break;
+    case (num == 5):
+      romanNum += "L";
+      break;
+    case (num > 5 && num < 9):
+      romanNum = "L";
+      romanNum += "X".repeat((num - 5));
+      break;
+    case (num == 9):
+      romanNum = "XC";
+      break;
+  }
+  return romanNum;
+}
+
+const handleI = num => {
+  if (num === 0) {return ""}
+  let romanNum = "";
+  switch (true) {
+    case (num < 4):
+      romanNum += "I".repeat(num);
+      break;
+    case (num == 4):
+      romanNum += "IV";
+      break;
+    case (num == 5):
+      romanNum += "V";
+      break;
+    case (num > 5 && num < 9):
+      romanNum = "V";
+      romanNum += "I".repeat((num-5));
+      break;
+    case (num == 9):
+      romanNum = "IX";
+      break;
+  }
+  return romanNum;
+}
