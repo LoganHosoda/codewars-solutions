ba61c5d23d049304cdc67fe05d918a6be305dae3
Logan Hosoda <sulsalogan@gmail.com>
2024-04-22 07:16:52 -0700

    Update human-readable-duration-format.js
    Refactored the human readable duration format code to be more concise using best-practices, covering more use-case scenarios as well.

--- 4kyu/human-readable-duration-format.js
+++ 4kyu/human-readable-duration-format.js
@@ -27,9 +27,28 @@
 
 // STRINGS DATE TIME ALGORITHMS
 
-// SOLUTION
+// REFACTORED SOLUTION
 
 function formatDuration (seconds) {
+  const time = {year: 31536000, day: 86400, hour: 3600, minute: 60, second: 1};
+  let result = [];
+  
+  if (seconds == 0) return "now";
+  
+  for (let key in time) {
+    if (seconds >= time[key]) {
+      let value = Math.floor(seconds / time[key]);
+      result.push(value > 1 ? value + ` ${key}s` : value + ` ${key}`);
+      seconds = seconds % time[key];
+    }
+  }
+  
+  return result.length > 1 ? result.join(", ").replace(/,([^,]*)$/,' and'+'$1') : result[0];
+}
+
+// ORIGINAL SOLUTION
+
+function originalFormatDuration (seconds) {
   if (seconds === 0) return "now";
   
   const year = 31536000;
@@ -80,8 +99,8 @@ function formatDuration (seconds) {
   if (hours == 1) ans += `${hours} hour`;
   if (hours > 1) ans += `${hours} hours`;
   // Addition of minutes
-  if (((years || days || hours) && minutes) && !sec) ans += ` and `
-  else if ((years || days || hours) && minutes) ans += `, `
+  if (((years || days || hours) && minutes) && !sec) ans += ` and `;
+  else if ((years || days || hours) && minutes) ans += `, `;
   if (minutes == 1) ans += `${minutes} minute`;
   if (minutes > 1) ans += `${minutes} minutes`;
   // Addition of seconds
