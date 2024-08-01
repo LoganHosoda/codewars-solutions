e1327d17e6e6ecb9734dbff238896d9b480715f4
Logan Hosoda <35867188+LoganHosoda@users.noreply.github.com>
2024-08-01 15:32:49 -0700

    solved 7kyu arrays kata

--- 7kyu/arrays/small-enough-beginner.js
+++ 7kyu/arrays/small-enough-beginner.js
@@ -1,5 +1,8 @@
-const smallEnough = (a, limit) => {
-  let ans = true;
-  a.forEach(el => el > limit ? ans = false : el  )
-  return ans
-}
+//const smallEnough = (a, limit) => {
+//  let ans = true;
+//  a.forEach(el => el > limit ? ans = false : el  )
+//  return ans
+//}
+
+// Refactored Solution 
+const smallEnough = (a, limit) => a.every(num => num <= limit)
