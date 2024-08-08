2a29d7bc67b957b93cc6508c84d50164e16156e9
Logan Hosoda <35867188+LoganHosoda@users.noreply.github.com>
2024-08-07 23:39:56 -0700

    solved 8kyu arrays kata

--- 8kyu/arrays/i-love-you-a-little.js
+++ 8kyu/arrays/i-love-you-a-little.js
@@ -1,5 +1,4 @@
 function howMuchILoveYou(num) {
-  let phrases = { 1: 'I love you', 2: 'a little', 3: 'a lot', 4: 'passionately', 5: 'madly', 6: 'not at all', 0: 'not at all' }
-  num > 6 ? num = Math.ceil(num % 6) : num
-  return phrases[num]
+  let phrases = { 0: 'not at all', 1: 'I love you', 2: 'a little', 3: 'a lot', 4: 'passionately', 5: 'madly' }
+  return phrases[num % 6]
 }
