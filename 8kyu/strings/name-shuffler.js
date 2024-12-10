e224a57b82974b23f94ad227c874af9be057cf6f
Logan Hosoda <35867188+LoganHosoda@users.noreply.github.com>
2024-12-10 15:00:55 -0800

    'Adding 8kyu strings kata name-shuffler.js'

---
+++ 8kyu/strings/name-shuffler.js
@@ -0,0 +1,7 @@
+// Solution using a funciton declaration
+function nameShuffler(str) {
+  return str.split(' ').reverse().join(' ')
+}
+
+// Reformatted to be one line
+const nameShuffler = str => str.split(' ').reverse().join(' ')
