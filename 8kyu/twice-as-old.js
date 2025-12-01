db8363a172d9a9fb2e277b57762edd040f9f9c50
Logan Hosoda <35867188+LoganHosoda@users.noreply.github.com>
2025-12-01 09:20:11 -0800

    'Completed 8kyu codewars challenge'

---
+++ 8kyu/twice-as-old.js
@@ -0,0 +1,7 @@
+// function twiceAsOld(dadYearsOld, sonYearsOld) {
+//   return Math.abs(sonYearsOld * 2 - dadYearsOld);
+// }
+
+// Shortened version:
+
+const twiceAsOld = (dad, son) => Math.abs(son * 2 - dad);
